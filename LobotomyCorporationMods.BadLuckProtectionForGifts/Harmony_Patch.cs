48c510915edbb15d926813a05f8db026e6548b25
CTristan <ctristan@gmail.com>
2020-10-04 09:42:15 -0700

    Added Harmony template for BadLuckProtectionForGifts

---
+++ LobotomyCorporationMods.BadLuckProtectionForGifts/Harmony_Patch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Harmony;
+using UnityEngine;
+
+// ReSharper disable InconsistentNaming
+namespace LobotomyCorporationMods.BadLuckProtectionForGifts
+{
+    public class Harmony_Patch
+    {
+        public Harmony_Patch()
+        {
+            try
+            {
+                var harmonyInstance = HarmonyInstance.Create("BadLuckProtectionForGifts");
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(Application.dataPath + "/BaseMods/BadLuckProtectionForGifts_Log.txt",
+                    ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+        }
+    }
+}
