a38a7cff67f6ca6da97c8e8fcf1b0b85657d05bb
Chris Tristan <chris.tristan@pm.me>
2022-07-19 05:45:45 -0700

    Initial version of ShowEffectAgentStats.

---
+++ LobotomyCorporation.ShowEffectiveAgentStats/Harmony_Patch.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using Harmony;
+using JetBrains.Annotations;
+using LobotomyCorporationMods.Common.Implementations;
+using LobotomyCorporationMods.Common.Interfaces;
+using UnityEngine;
+
+namespace LobotomyCorporation.ShowEffectiveAgentStats
+{
+    [SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores")]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public sealed class Harmony_Patch
+    {
+        private static IFile File;
+        private static string LogFile;
+
+        /// <summary>
+        ///     Do not use for testing as it causes an exception. Use the other constructor instead.
+        /// </summary>
+        public Harmony_Patch()
+        {
+            var dataPath = Application.dataPath + @"/BaseMods/ShowEffectiveAgentStats/";
+            Initialize(dataPath);
+            InitializeHarmonyPatch();
+        }
+
+        /// <summary>
+        ///     Entry point for testing.
+        /// </summary>
+        public Harmony_Patch(string dataPath)
+        {
+            Initialize(dataPath);
+        }
+
+        /// <summary>
+        ///     Loads data files.
+        /// </summary>
+        private static void Initialize(string dataPath)
+        {
+            File = new File();
+            LogFile = dataPath + "ShowEffectiveAgentStats_Log.txt";
+        }
+
+        /// <summary>
+        ///     Patches all of the relevant method calls through Harmony.
+        /// </summary>
+        private static void InitializeHarmonyPatch()
+        {
+            try
+            {
+                var harmonyInstance = HarmonyInstance.Create("ShowEffectiveAgentStats");
+                if (harmonyInstance == null)
+                {
+                    throw new InvalidOperationException(nameof(harmonyInstance));
+                }
+
+                var harmonyMethod = new HarmonyMethod(typeof(Harmony_Patch).GetMethod("SetStatPostfix"));
+                harmonyInstance.Patch(typeof(AgentInfoWindow.WorkerPrimaryStatUI).GetMethod("SetStat", AccessTools.all),
+                    null, harmonyMethod);
+            }
+            catch (Exception ex)
+            {
+                WriteToLog(File, ex.Message + Environment.NewLine + ex.StackTrace);
+                throw;
+            }
+        }
+
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
+        public static void SetStat([NotNull] AgentInfoWindow.WorkerPrimaryStatUI __instance, AgentModel agent)
+        {
+            var statName = string.Empty;
+            var statValue = 0;
+            var statType = __instance.type;
+            var statEffectList = __instance.list;
+
+            switch (statType)
+            {
+                case RwbpType.R:
+                    {
+                        statName = "Rstat";
+                        statValue = agent.Rstat;
+
+                        var originalValue = agent.primaryStat.maxHP + agent.titleBonus.maxHP;
+                        var additionalValue = agent.maxHp - originalValue;
+                        var workExperience = Mathf.FloorToInt(agent.primaryStatExp.hp);
+                        SetStatText(statEffectList[0], originalValue, additionalValue, workExperience);
+
+                        break;
+                    }
+            }
+
+            __instance.StatName.text = string.Format(CultureInfo.CurrentCulture, "{0} {1}",
+                LocalizeTextDataModel.instance.GetText(statName), AgentModel.GetLevelGradeText(statValue));
+        }
+
+        private static void SetStatText([NotNull] AgentInfoWindow.WorkerPrimaryStatUnit statUnit, int originalValue,
+            int additionalValue, int experienceValue)
+        {
+            if (additionalValue == 0)
+            {
+                statUnit.StatValue.text = originalValue.ToString(CultureInfo.CurrentCulture);
+
+                if (experienceValue > 0)
+                {
+                    statUnit.StatValue.text += string.Format(CultureInfo.CurrentCulture, @"+{0}", experienceValue);
+                }
+
+                return;
+            }
+
+            var effectiveValue = originalValue + additionalValue;
+            var combinedValue = effectiveValue + experienceValue;
+            string additionalValueColor;
+            string operation;
+
+            if (additionalValue > 0)
+            {
+                additionalValueColor = AgentInfoWindow.currentWindow.Additional_Plus_ValueColor;
+                operation = "+";
+            }
+            else
+            {
+                additionalValueColor = AgentInfoWindow.currentWindow.Additional_Minus_ValueColor;
+                operation = "-";
+            }
+
+            var template = @"{0}/{1} ({2}<color=#{3}>{4}{5}</color>+{6})";
+            statUnit.StatValue.text = string.Format(template, effectiveValue, combinedValue, originalValue,
+                additionalValueColor, operation, Mathf.Abs(additionalValue), experienceValue);
+        }
+
+        /// <summary>
+        ///     Writes to a log file.
+        /// </summary>
+        /// <param name="file">The file interface.</param>
+        /// <param name="message">The message to log.</param>
+        private static void WriteToLog([NotNull] IFile file, [NotNull] string message)
+        {
+            file.WriteAllText(LogFile, message);
+        }
+    }
+}
--- LobotomyCorporationMods.Test/TestExtensions.cs
+++ LobotomyCorporationMods.Test/TestExtensions.cs
@@ -1,16 +1,159 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.Serialization;
+using JetBrains.Annotations;
+using NSubstitute;
+using UnityEngine.UI;
 
 namespace LobotomyCorporationMods.Test
 {
     internal static class TestExtensions
     {
+        [NotNull]
+        public static AgentInfoWindow.WorkerPrimaryStatUI CreateAgentInfoWindow_WorkerPrimaryStatUI(RwbpType type)
+        {
+            var ui = Substitute.For<AgentInfoWindow.WorkerPrimaryStatUI>();
+            ui.type = type;
+            ui.StatName = Substitute.For<Text>();
+            ui.list = new List<AgentInfoWindow.WorkerPrimaryStatUnit>
+            {
+                new AgentInfoWindow.WorkerPrimaryStatUnit { StatValue = Substitute.For<Text>() }
+            };
+
+            return ui;
+        }
+
+        [NotNull]
+        public static AgentModel CreateAgentModel(long id, int primaryStat = 1, int egoBonus = 0,
+            float primaryStatExp = 0f)
+        {
+            var agent = CreateUninitializedObject<AgentModel>();
+            agent.instanceId = id;
+            agent.primaryStat = new WorkerPrimaryStat
+            {
+                battle = primaryStat, hp = primaryStat, mental = primaryStat, work = primaryStat
+            };
+            agent.primaryStatExp = new WorkerPrimaryStatExp
+            {
+                battle = primaryStatExp, hp = primaryStatExp, mental = primaryStatExp, work = primaryStatExp
+            };
+
+
+            var newAgent = CreateUninitializedObject<AgentModel>();
+            var fields = GetUninitializedObjectFields(agent.GetType());
+            var newValues = new Dictionary<string, object>
+            {
+                { "_statBufList", new List<UnitStatBuf>() },
+                {
+                    "_equipment",
+                    new UnitEquipSpace
+                    {
+                        gifts = new UnitEGOgiftSpace
+                        {
+                            addedGifts = new List<EGOgiftModel>
+                            {
+                                EGOgiftModel.MakeGift(new EquipmentTypeInfo
+                                {
+                                    bonus = new EGObonusInfo
+                                    {
+                                        damage = egoBonus,
+                                        attackSpeed = egoBonus,
+                                        cubeSpeed = egoBonus,
+                                        hp = egoBonus,
+                                        mental = egoBonus,
+                                        movement = egoBonus,
+                                        workProb = egoBonus
+                                    }
+                                })
+                            }
+                        }
+                    }
+                }
+            };
+            agent = GetPopulatedUninitializedObject(agent, fields, newValues);
+
+            return agent;
+        }
+
+        [NotNull]
+        public static CreatureEquipmentMakeInfo CreateCreatureEquipmentMakeInfo(string giftName)
+        {
+            var info = Substitute.For<CreatureEquipmentMakeInfo>();
+            info.equipTypeInfo = new EquipmentTypeInfo
+            {
+                localizeData = new Dictionary<string, string> { { "name", giftName } },
+                type = EquipmentTypeInfo.EquipmentType.SPECIAL
+            };
+
+            LocalizeTextDataModel.instance?.Init(new Dictionary<string, string> { { giftName, giftName } });
+
+            return info;
+        }
+
+        [NotNull]
+        public static UseSkill CreateUseSkill(string giftName, long agentId, int numberOfSuccesses)
+        {
+            var useSkill = Substitute.For<UseSkill>();
+            useSkill.agent = CreateUninitializedObject<AgentModel>();
+            useSkill.agent.instanceId = agentId;
+            useSkill.targetCreature = CreateUninitializedObject<CreatureModel>();
+            useSkill.targetCreature.metaInfo = new CreatureTypeInfo
+            {
+                equipMakeInfos = new List<CreatureEquipmentMakeInfo> { CreateCreatureEquipmentMakeInfo(giftName) }
+            };
+            useSkill.successCount = numberOfSuccesses;
+
+            return useSkill;
+        }
+
+        #region Uninitialized Object Functions
+
         /// <summary>
         ///     Create an uninitialized object without calling a constructor. Needed because some of the classes we need
         ///     to mock either don't have a public constructor or cause a Unity exception.
         /// </summary>
-        public static TObject CreateUninitializedObject<TObject>()
+        private static TObject CreateUninitializedObject<TObject>()
         {
             return (TObject)FormatterServices.GetSafeUninitializedObject(typeof(TObject));
         }
+
+        [NotNull]
+        private static MemberInfo[] GetUninitializedObjectFields(Type type)
+        {
+            var fields = new List<MemberInfo>();
+
+            while (type != typeof(object))
+            {
+                fields.AddRange(type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance |
+                                               BindingFlags.DeclaredOnly));
+                type = type?.BaseType;
+            }
+
+            return fields.ToArray();
+        }
+
+        [NotNull]
+        private static TObject GetPopulatedUninitializedObject<TObject>(TObject obj, MemberInfo[] fields,
+            Dictionary<string, object> newValues)
+        {
+            var newObj = CreateUninitializedObject<TObject>();
+            var values = FormatterServices.GetObjectData(obj, fields.ToArray());
+
+            for (var i = 0; i < fields.Length; i++)
+            {
+                if (newValues.ContainsKey(fields[i].Name))
+                {
+                    values[i] = newValues[fields[i].Name];
+                }
+            }
+
+            FormatterServices.PopulateObjectMembers(newObj, fields.ToArray(), values);
+
+            return newObj;
+        }
+
+        #endregion
     }
 }
