d550b4a07d384a6b93f0489b960073ce827f3f8b
Chris Tristan <chris.tristan@pm.me>
2022-07-23 15:17:20 -0700

    Initial proof of concept for ForceDayEndAfterMaxMeltdown

---
+++ LobotomyCorporationMods.ForceDayEndAfterMaxMeltdownLevel/Extensions/ModExtensions.cs
@@ -0,0 +1,54 @@
+using System.IO;
+using JetBrains.Annotations;
+
+namespace LobotomyCorporationMods.ForceDayEndAfterMaxMeltdownLevel.Extensions
+{
+    internal static class ModExtensions
+    {
+        [NotNull] private static readonly object s_fileLock = new object();
+
+        [NotNull]
+        public static string GetDataPath(string modFileName)
+        {
+            foreach (var directoryInfo in Add_On.instance.DirList)
+            {
+                if (File.Exists(Path.Combine(directoryInfo.FullName, modFileName)))
+                {
+                    return directoryInfo.FullName;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        [NotNull]
+        public static string ReadAllText([NotNull] string path)
+        {
+            return ReadAllText(path, false);
+        }
+
+        [NotNull]
+        public static string ReadAllText([NotNull] string path, bool createIfNotExists)
+        {
+            if (!File.Exists(path))
+            {
+                if (!createIfNotExists) { return string.Empty; }
+
+                WriteAllText(path, string.Empty);
+            }
+
+            lock (s_fileLock)
+            {
+                return File.ReadAllText(path);
+            }
+        }
+
+        public static void WriteAllText([NotNull] string path, [NotNull] string contents)
+        {
+            lock (s_fileLock)
+            {
+                File.WriteAllText(path, contents);
+            }
+        }
+    }
+}
---
+++ LobotomyCorporationMods.ForceDayEndAfterMaxMeltdownLevel/Harmony_Patch.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Harmony;
+using JetBrains.Annotations;
+using LobotomyCorporationMods.ForceDayEndAfterMaxMeltdownLevel.Extensions;
+
+#pragma warning disable CA1707
+namespace LobotomyCorporationMods.ForceDayEndAfterMaxMeltdownLevel
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public sealed class Harmony_Patch
+    {
+        private const string ModFileName = "LobotomyCorporationMods.ForceDayEndAfterMaxMeltdownLevel.dll";
+
+        private static string DataPath;
+        private static string LogFile;
+
+        /// <summary>
+        ///     Do not use for testing as it causes an exception. Use the other constructor instead.
+        /// </summary>
+        public Harmony_Patch()
+        {
+            DataPath = ModExtensions.GetDataPath(ModFileName);
+
+            Initialize();
+            InitializeHarmonyPatch();
+        }
+
+        /// <summary>
+        ///     Entry point for testing.
+        /// </summary>
+        public Harmony_Patch(string dataPath)
+        {
+            DataPath = dataPath;
+
+            Initialize();
+        }
+
+        /// <summary>
+        ///     Loads data files.
+        /// </summary>
+        private static void Initialize()
+        {
+            LogFile = Path.Combine(DataPath, "log.txt");
+        }
+
+        /// <summary>
+        ///     Patches all of the relevant method calls through Harmony.
+        /// </summary>
+        private static void InitializeHarmonyPatch()
+        {
+            try
+            {
+                var harmonyInstance = HarmonyInstance.Create("ForceDayEndAfterMaxMeltdownLevel");
+                if (harmonyInstance == null)
+                {
+                    throw new InvalidOperationException(nameof(harmonyInstance));
+                }
+
+                var harmonyMethod = new HarmonyMethod(typeof(Harmony_Patch).GetMethod("AddOverloadGaguePrefix"));
+                harmonyInstance.Patch(typeof(CreatureOverloadManager).GetMethod("AddOverloadGague", new Type[] { }),
+                    harmonyMethod, null);
+            }
+            catch (Exception ex)
+            {
+                ModExtensions.WriteAllText(LogFile, ex.Message + Environment.NewLine + ex.StackTrace);
+                throw;
+            }
+        }
+
+        /// <summary>
+        ///     Runs before the Meltdown Counter is incremented to see if we're already at the highest meltdown level. If
+        ///     we are and we aren't in an emergency then forcefully end the day.
+        /// </summary>
+        // ReSharper disable once IdentifierTypo
+        public static bool AddOverloadGaguePrefix([NotNull] CreatureOverloadManager __instance)
+        {
+            const int maxMeltdownLevel = 10;
+            var meltdownLevel = __instance.GetQliphothOverloadLevel();
+
+            // If we're not at the max level then go through the normal method
+            if (meltdownLevel < maxMeltdownLevel) { return true; }
+
+            var gameManager = GameManager.currentGameManager;
+
+            // In the interest of fairness, we'll allow the player to clear any emergency before ending the day
+            if (!gameManager.emergency)
+            {
+                gameManager.ClearStage();
+            }
+
+            return false;
+        }
+    }
+}
+#pragma warning restore CA1707
