eb227fa4652daf53de682bd3c627ac0d7614c098
Chris Tristan <chris.tristan@protonmail.com>
2022-07-25 19:16:54 -0700

    Added additional tests for end stage logic.

--- LobotomyCorporationMods.Test/ForceDayEndAfterMaxMeltdownLevelTests.cs
+++ LobotomyCorporationMods.Test/ForceDayEndAfterMaxMeltdownLevelTests.cs
@@ -16,6 +16,10 @@ namespace LobotomyCorporationMods.Test
         {
             var fileManager = TestExtensions.GetFileManager();
             _ = new Harmony_Patch(fileManager);
+
+            // Static instances that need to be created before some of the tests will work
+            _ = TestExtensions.CreateGlobalGameManager();
+            _ = TestExtensions.CreateEnergyModel(999f);
         }
 
         [Fact]
@@ -55,6 +59,40 @@ namespace LobotomyCorporationMods.Test
 
             // Assert
             result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void AddOverloadGaguePrefix_MaxMeltdownLevelAndMaxQliphothCounterButInEmergency_StageEndedIsFalse()
+        {
+            // Arrange
+            const int OverloadMax = 0;
+            var creatureOverloadManager = TestExtensions.CreateCreatureOverloadManager(MaxLevel, OverloadMax);
+            var gameManager = TestExtensions.CreateGameManager(true);
+
+            // Act
+            var result = Harmony_Patch.AddOverloadGaguePrefix(creatureOverloadManager);
+
+            // Assert
+            result.Should().BeFalse();
+            gameManager.StageEnded.Should().BeFalse();
+        }
+
+        /// <summary>
+        ///     This is untestable because the actual method to end the day calls another game method that Unity won't allow.
+        /// </summary>
+        [Fact]
+        public void AddOverloadGaguePrefix_MaxMeltdownLevelAndMaxQliphothCounterAndNotInEmergency_InUntestable()
+        {
+            // Arrange
+            const int OverloadMax = 0;
+            var creatureOverloadManager = TestExtensions.CreateCreatureOverloadManager(MaxLevel, OverloadMax);
+            var gameManager = TestExtensions.CreateGameManager();
+
+            // Act
+            var result = Record.Exception(() => Harmony_Patch.AddOverloadGaguePrefix(creatureOverloadManager));
+
+            // Assert
+            TestExtensions.AssertIsUnityException(result).Should().BeTrue();
         }
     }
 }
