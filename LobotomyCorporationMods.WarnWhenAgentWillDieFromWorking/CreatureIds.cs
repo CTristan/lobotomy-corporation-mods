3ab3fd15ef38defbeb93f6eb19d5bad19386b9ce
Chris Tristan <chris.tristan@protonmail.com>
2022-07-30 19:21:40 -0700

    Added Spider Bud, Void Machine, and Warm-Hearted Woodsman.

--- LobotomyCorporationMods.WarnWhenAgentWillDieFromWorking/CreatureIds.cs
+++ LobotomyCorporationMods.WarnWhenAgentWillDieFromWorking/CreatureIds.cs
@@ -2,6 +2,9 @@ namespace LobotomyCorporationMods.WarnWhenAgentWillDieFromWorking
 {
     public enum CreatureIds
     {
-        SingingMachine = 100006
+        SingingMachine = 100006,
+        SpiderBud = 100012,
+        VoidMachine = 100060,
+        WarmHeartedWoodsman = 100051
     }
 }
--- LobotomyCorporationMods.WarnWhenAgentWillDieFromWorking/Harmony_Patch.cs
+++ LobotomyCorporationMods.WarnWhenAgentWillDieFromWorking/Harmony_Patch.cs
@@ -5,7 +5,6 @@ using Harmony;
 using JetBrains.Annotations;
 using LobotomyCorporationMods.Common.Implementations;
 using LobotomyCorporationMods.Common.Interfaces;
-using Object = UnityEngine.Object;
 
 namespace LobotomyCorporationMods.WarnWhenAgentWillDieFromWorking
 {
@@ -61,36 +60,66 @@ namespace LobotomyCorporationMods.WarnWhenAgentWillDieFromWorking
             {
                 var commandWindow = CommandWindow.CommandWindow.CurrentWindow;
 
-                // if (!creature.observeInfo.IsMaxObserved())
-                // {
-                //     return;
-                // }
                 if (state == AgentState.DEAD || state == AgentState.PANIC || state == AgentState.UNCONTROLLABLE)
                 {
                     return;
                 }
 
-                if (!(bool)(Object)CommandWindow.CommandWindow.CurrentWindow)
+                if (commandWindow.CurrentWindowType != CommandType.Management)
                 {
                     return;
                 }
 
-                if (CommandWindow.CommandWindow.CurrentWindow.CurrentWindowType != CommandType.Management)
+                if (!(commandWindow.CurrentTarget is CreatureModel creature)) { return; }
+
+                if (!creature.observeInfo.IsMaxObserved())
                 {
                     return;
                 }
 
-                var creature = commandWindow.WorkAllocate.CurrentModel;
                 var agent = __instance.CurrentAgent;
                 var agentWillDie = false;
                 var qliphothCounter = creature.qliphothCounter;
 
-                if (creature.metadataId == (long)CreatureIds.SingingMachine)
+                switch (creature.metadataId)
                 {
-                    if (qliphothCounter == 0 || agent.fortitudeLevel >= 4 || agent.temperanceLevel <= 2)
-                    {
-                        agentWillDie = true;
-                    }
+                    case (long)CreatureIds.SingingMachine:
+                        {
+                            /*
+                             Singing Machine's gift increases the Fortitude stat, and since the kill agent check is at
+                             the end of the work session it's possible for the agent to get the gift which increases the
+                             Fortitude level from 3 to 4. To account for that we look at the actual stat value instead
+                             of the level.
+                             */
+                            const int GiftIncrease = 8;
+                            const int FortitudeThreshold = 65;
+                            var fortitudeStatTooHigh = agent.fortitudeStat >= FortitudeThreshold - GiftIncrease;
+
+                            if (qliphothCounter == 0 || fortitudeStatTooHigh || agent.temperanceLevel <= 2)
+                            {
+                                agentWillDie = true;
+                            }
+
+                            break;
+                        }
+                    case (long)CreatureIds.SpiderBud:
+                        {
+                            agentWillDie = agent.prudenceLevel == 1;
+
+                            break;
+                        }
+                    case (long)CreatureIds.VoidMachine:
+                        {
+                            agentWillDie = agent.temperanceLevel < 2;
+
+                            break;
+                        }
+                    case (long)CreatureIds.WarmHeartedWoodsman:
+                        {
+                            agentWillDie = qliphothCounter == 0;
+
+                            break;
+                        }
                 }
 
                 if (agentWillDie)
