325bda62dcfc3e17e3ec9f43cf1ffe8778924b2c
Chris Tristan <chris.tristan@protonmail.com>
2023-01-16 19:25:50 -0700

    Updated readme

--- LobotomyCorporationMods.Test/BugFixesTests.cs
+++ LobotomyCorporationMods.Test/BugFixesTests.cs
@@ -19,6 +19,8 @@ namespace LobotomyCorporationMods.Test
             _ = new Harmony_Patch();
         }
 
+        #region BugFixStatUpgrades
+
         [Theory]
         [InlineData(1, 30, 37)]
         [InlineData(2, 45, 55)]
@@ -36,6 +38,8 @@ namespace LobotomyCorporationMods.Test
 
             result.Should().BeGreaterOrEqualTo(minNextStatValue).And.BeLessOrEqualTo(maxNextStatValue);
         }
+
+        #endregion
 
         #region Helper Methods
 
