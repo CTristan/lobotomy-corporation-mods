1d066ff61cdf0523d7313b549103595507b1e80a
Chris Tristan <chris.tristan@protonmail.com>
2023-01-21 04:55:04 -0700

    Cleaned up Assembly Info locations

---
+++ LobotomyCorporationMods.FreeCustomization/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
