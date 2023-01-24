2ab7bbbcb66bd10a584ff12677d0019522542d06
Chris Tristan <chris.tristan@protonmail.com>
2023-01-24 08:37:54 -0700

    Added unit test for verifying that the message is sent.

--- LobotomyCorporationMods.NotifyWhenGiftReceived/Patches/UnitModelPatchAttachEgoGift.cs
+++ LobotomyCorporationMods.NotifyWhenGiftReceived/Patches/UnitModelPatchAttachEgoGift.cs
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 
+using System;
 using System.Diagnostics.CodeAnalysis;
 using Harmony;
 using JetBrains.Annotations;
+using LobotomyCorporationMods.Common.Extensions;
+using LobotomyCorporationMods.Common.Implementations;
 using LobotomyCorporationMods.NotifyWhenGiftReceived.Extensions;
 
 namespace LobotomyCorporationMods.NotifyWhenGiftReceived.Patches
@@ -13,14 +16,38 @@ namespace LobotomyCorporationMods.NotifyWhenGiftReceived.Patches
     public static class UnitModelPatchAttachEgoGift
     {
         // ReSharper disable once InconsistentNaming
-        public static void Prefix([NotNull] UnitModel __instance, [CanBeNull] EGOgiftModel gift)
+        public static void Prefix([NotNull] UnitModel __instance, [NotNull] EGOgiftModel gift)
         {
-            // If we already have this gift equipped we don't want to send an unnecessary notification
-            if (__instance.HasGiftEquipped(gift.metaInfo.id)) { return; }
+            try
+            {
+                Guard.Against.Null(__instance, nameof(__instance));
+                Guard.Against.Null(gift, nameof(gift));
 
-            // Send notification that the agent acquired the gift
-            var message = $"<color=#66bfcd>{__instance.GetUnitName()}</color> has received the gift <color=#84bd36>{gift.metaInfo.Name}</color>.";
-            Notice.instance.Send("AddSystemLog", message);
+                // If we already have this gift equipped we don't want to send an unnecessary notification
+                if (__instance.HasGiftEquipped(gift.metaInfo.id))
+                {
+                    return;
+                }
+
+                // TODO: Check for locked gift
+
+                // Send notification that the agent acquired the gift
+                var message = $"<color=#66bfcd>{__instance.GetUnitName()}</color> has received the gift <color=#84bd36>{gift.metaInfo.Name}</color>.";
+                Notice.instance.Send("AddSystemLog", message);
+            }
+            catch (Exception ex)
+            {
+                // Null argument exception only comes up during testing due to Unity operator overloading.
+                // https://github.com/JetBrains/resharper-unity/wiki/Possible-unintended-bypass-of-lifetime-check-of-underlying-Unity-engine-object
+                if (ex is ArgumentNullException)
+                {
+                    return;
+                }
+
+                Harmony_Patch.Instance.FileManager.WriteToLog(ex);
+
+                throw;
+            }
         }
     }
 }
