d99e87fef736ca46cf533185dc7094f23f4e3d01
Chris Tristan <chris.tristan@protonmail.com>
2023-01-25 07:54:34 -0700

    Refactored unit tests to be more maintainable.

--- LobotomyCorporationMods.Test/Extensions/TestExtensions.cs
+++ LobotomyCorporationMods.Test/Extensions/TestExtensions.cs
@@ -3,16 +3,15 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
-using System.Security;
 using Customizing;
 using FluentAssertions;
 using Harmony;
 using JetBrains.Annotations;
 using LobotomyCorporationMods.Common.Interfaces;
 using NSubstitute;
-using UnityEngine;
 
 // ReSharper disable once CheckNamespace
 namespace LobotomyCorporationMods.Test
@@ -35,16 +34,6 @@ namespace LobotomyCorporationMods.Test
             return Path.Combine(Directory.GetCurrentDirectory(), fileName);
         }
 
-        /// <summary>
-        ///     Depending on the environment, the same test may return a different exception. Both exception types appear for the
-        ///     same reason (trying to use a Unity-specific method), so we'll check if the exception we get is either of those
-        ///     types to verify our test isn't getting an exception for some other reason.
-        /// </summary>
-        public static bool AssertIsUnityException(Exception exception)
-        {
-            return exception is SecurityException || exception is MissingMethodException;
-        }
-
         public static void ValidateHarmonyPatch([NotNull] this MemberInfo patchClass, [NotNull] Type originalClass, string methodName)
         {
             var attribute = Attribute.GetCustomAttribute(patchClass, typeof(HarmonyPatch)) as HarmonyPatch;
@@ -53,19 +42,6 @@ namespace LobotomyCorporationMods.Test
             attribute?.info.originalType.Should().Be(originalClass);
             attribute?.info.methodName.Should().Be(methodName);
         }
-
-        #region Uninitialized Object Functions
-
-        /// <summary>
-        ///     Create an uninitialized object without calling a constructor. Needed because some of the classes we need
-        ///     to mock either don't have a public constructor or cause a Unity exception.
-        /// </summary>
-        private static void CreateUninitializedObject<TObject>(out TObject obj)
-        {
-            obj = (TObject)FormatterServices.GetSafeUninitializedObject(typeof(TObject));
-        }
-
-        #endregion
 
         #region Unity Objects
 
@@ -89,14 +65,6 @@ namespace LobotomyCorporationMods.Test
         }
 
         [NotNull]
-        public static GameObject CreateGameObject()
-        {
-            CreateUninitializedObject(out GameObject obj);
-
-            return obj;
-        }
-
-        [NotNull]
         public static UseSkill CreateUseSkill(string giftName, long agentId, int numberOfSuccesses)
         {
             var useSkill = Substitute.For<UseSkill>();
@@ -107,6 +75,89 @@ namespace LobotomyCorporationMods.Test
             useSkill.successCount = numberOfSuccesses;
 
             return useSkill;
+        }
+
+        #endregion
+
+        #region Uninitialized Object Functions
+
+        /// <summary>
+        ///     Create an uninitialized object without calling a constructor. Needed because some of the classes we need
+        ///     to mock either don't have a public constructor or cause a Unity exception.
+        /// </summary>
+        private static void CreateUninitializedObject<TObject>(out TObject obj)
+        {
+            obj = (TObject)FormatterServices.GetSafeUninitializedObject(typeof(TObject));
+        }
+
+        /// <summary>
+        ///     Get the fields for an uninitialized object. Can be used to later initialize the individual fields as needed.
+        /// </summary>
+        [NotNull]
+        private static MemberInfo[] GetUninitializedObjectFields(Type type)
+        {
+            var fields = new List<MemberInfo>();
+
+            while (type != typeof(object))
+            {
+                if (type == null)
+                {
+                    continue;
+                }
+
+                var typeFields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Static);
+                fields.AddRange(typeFields.GetValidFields());
+                type = type.BaseType;
+            }
+
+            return fields.ToArray();
+        }
+
+        /// <summary>
+        ///     Returns the fields which are valid and can be used to populate an object.
+        /// </summary>
+        [NotNull]
+        private static IEnumerable<FieldInfo> GetValidFields([NotNull] this IEnumerable<FieldInfo> typeFields)
+        {
+            var goodFields = new List<FieldInfo>();
+            foreach (var typeField in typeFields)
+            {
+                try
+                {
+                    if (typeField.FieldHandle.Value != IntPtr.Zero)
+                    {
+                        goodFields.Add(typeField);
+                    }
+                }
+                catch
+                {
+                    // Some fields will give an exception when checked, so we won't be able to populate those fields
+                }
+            }
+
+            return goodFields;
+        }
+
+        /// <summary>
+        ///     Populate the fields of an uninitialized object with a provided list of objects.
+        /// </summary>
+        [NotNull]
+        private static TObject GetPopulatedUninitializedObject<TObject>([NotNull] TObject obj, [NotNull] MemberInfo[] fields, IDictionary<string, object> newValues)
+        {
+            CreateUninitializedObject<TObject>(out var newObj);
+            var values = FormatterServices.GetObjectData(obj, fields.ToArray());
+
+            for (var i = 0; i < fields.Length; i++)
+            {
+                if (newValues.ContainsKey(fields[i].Name))
+                {
+                    values[i] = newValues[fields[i].Name];
+                }
+            }
+
+            FormatterServices.PopulateObjectMembers(newObj, fields.ToArray(), values);
+
+            return newObj;
         }
 
         #endregion
