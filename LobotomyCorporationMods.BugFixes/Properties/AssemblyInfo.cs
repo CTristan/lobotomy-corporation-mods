2575de4e7111fbe37489b507ad82f61bcf34cff6
Chris Tristan <chris.tristan@protonmail.com>
2023-02-25 07:21:46 -0700

    Updated tests to match current testing standards.

--- LobotomyCorporationMods.BugFixes/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: CLSCompliant(false)]
-[assembly: ComVisible(false)]
