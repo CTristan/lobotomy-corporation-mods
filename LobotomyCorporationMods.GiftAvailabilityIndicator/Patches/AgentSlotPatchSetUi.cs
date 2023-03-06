137e2d14ec0e992b67d7b84afbfd916345144697
Chris Tristan <chris.tristan@protonmail.com>
2023-03-05 18:41:01 -0700

    Fixed error when opening the suppression window

--- LobotomyCorporationMods.GiftAvailabilityIndicator/Patches/AgentSlotPatchSetUi.cs
+++ LobotomyCorporationMods.GiftAvailabilityIndicator/Patches/AgentSlotPatchSetUi.cs
@@ -51,41 +51,41 @@ namespace LobotomyCorporationMods.GiftAvailabilityIndicator.Patches
         {
             Image image;
 
-            switch (instance.name)
-            {
-                case SlotOneName:
-                    s_slotOneImage ??= instance.CreateGiftAvailabilityImage();
-                    image = s_slotOneImage;
-
-                    break;
-                case SlotTwoName:
-                    s_slotTwoImage ??= instance.CreateGiftAvailabilityImage();
-                    image = s_slotTwoImage;
-
-                    break;
-                case SlotThreeName:
-                    s_slotThreeImage ??= instance.CreateGiftAvailabilityImage();
-                    image = s_slotThreeImage;
-
-                    break;
-                case SlotFourName:
-                    s_slotFourImage ??= instance.CreateGiftAvailabilityImage();
-                    image = s_slotFourImage;
-
-                    break;
-                case SlotFiveName:
-                    s_slotFiveImage ??= instance.CreateGiftAvailabilityImage();
-                    image = s_slotFiveImage;
-
-                    break;
-                default:
-                    throw new InvalidOperationException(instance.name + " is not a valid slot name");
-            }
-
             var commandWindow = CommandWindow.CommandWindow.CurrentWindow;
 
-            if (commandWindow is not null)
+            if (commandWindow is not null && commandWindow.CurrentWindowType == CommandType.Management)
             {
+                switch (instance.name)
+                {
+                    case SlotOneName:
+                        s_slotOneImage ??= instance.CreateGiftAvailabilityImage();
+                        image = s_slotOneImage;
+
+                        break;
+                    case SlotTwoName:
+                        s_slotTwoImage ??= instance.CreateGiftAvailabilityImage();
+                        image = s_slotTwoImage;
+
+                        break;
+                    case SlotThreeName:
+                        s_slotThreeImage ??= instance.CreateGiftAvailabilityImage();
+                        image = s_slotThreeImage;
+
+                        break;
+                    case SlotFourName:
+                        s_slotFourImage ??= instance.CreateGiftAvailabilityImage();
+                        image = s_slotFourImage;
+
+                        break;
+                    case SlotFiveName:
+                        s_slotFiveImage ??= instance.CreateGiftAvailabilityImage();
+                        image = s_slotFiveImage;
+
+                        break;
+                    default:
+                        throw new InvalidOperationException(instance.name + " is not a valid slot name");
+                }
+
                 var abnormalityGift = commandWindow.GetCreatureGiftIfExists();
 
                 if (abnormalityGift is not null)
