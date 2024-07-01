511703d421f16bdab4ed25c39dda805cd259e429
Chris Tristan <chris.tristan@protonmail.com>
2024-07-01 06:23:20 -0700

    Refactor code to improve modularity and organization
    Numerous instances of the 'UseSkill' related code are removed from 'AbnormalityFacade.cs' and 'AgentFacade.cs' and moved into separate extension classes, namely 'UseSkillExtensions.cs' and 'CreatureEquipmentMakeInfoExtensions.cs'. This provides a cleaner, more organized codebase and also increases modularity for future improvements and modifications.

--- LobotomyCorporationMods.Common/Implementations/Facades/AgentFacade.cs
+++ LobotomyCorporationMods.Common/Implementations/Facades/AgentFacade.cs
@@ -3,18 +3,10 @@
 using JetBrains.Annotations;
 using LobotomyCorporationMods.Common.Extensions;
 
-// ReSharper disable MemberCanBePrivate.Global
 namespace LobotomyCorporationMods.Common.Implementations.Facades
 {
     public static class AgentFacade
     {
-        public static AgentModel GetAgent([NotNull] this UseSkill useSkill)
-        {
-            Guard.Against.Null(useSkill, nameof(useSkill));
-
-            return useSkill.agent;
-        }
-
         public static long GetAgentId([NotNull] this UseSkill useSkill)
         {
             return useSkill.GetAgent().instanceId;
